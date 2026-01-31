93170077d76a6d60497066cbfc3f2b430e56f78a
movte <movtery228@qq.com>
2026-02-01 00:33:20 +0800

    1.1>=26.1

--- src/main/java/com/movtery/betterf1/mixin/GameRendererMixin.java
+++ src/main/java/com/movtery/betterf1/mixin/GameRendererMixin.java
@@ -2,9 +2,9 @@ package com.movtery.betterf1.mixin;
 
 import com.movtery.betterf1.client.BetterF1;
 import com.movtery.betterf1.client.HUDState;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,24 +12,24 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = GameRenderer.class)
+@Mixin(GameRenderer.class)
 public class GameRendererMixin {
     @Shadow
     @Final
-    private MinecraftClient client;
+    private Minecraft minecraft;
 
     // Doing it this way is for Optifine compatibility
-    @Inject(method = "renderWorld", at = @At("HEAD"))
-    private void onRenderHand1(RenderTickCounter tickCounter, CallbackInfo ci) {
+    @Inject(method = "renderLevel", at = @At("HEAD"))
+    private void onRenderHand1(DeltaTracker tickCounter, CallbackInfo ci) {
         if (BetterF1.state.equals(HUDState.NO_HUD)) {
-            client.options.hudHidden = false;
+            minecraft.options.hideGui = false;
         }
     }
 
-    @Inject(method = "renderWorld", at = @At("TAIL"))
+    @Inject(method = "renderLevel", at = @At("TAIL"))
     private void onRenderHand2(CallbackInfo ci) {
         if (BetterF1.state.equals(HUDState.NO_HUD)) {
-            client.options.hudHidden = true;
+            minecraft.options.hideGui = true;
         }
     }
 }
--- src/main/java/com/movtery/betterf1/mixin/KeyboardHandlerMixin.java
+++ src/main/java/com/movtery/betterf1/mixin/KeyboardHandlerMixin.java
@@ -2,8 +2,8 @@ package com.movtery.betterf1.mixin;
 
 import com.movtery.betterf1.client.BetterF1;
 import com.movtery.betterf1.client.HUDState;
-import net.minecraft.client.Keyboard;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,19 +11,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Keyboard.class)
-public class KeyboardMixin {
+@Mixin(KeyboardHandler.class)
+public class KeyboardHandlerMixin {
     @Shadow
     @Final
-    private MinecraftClient client;
+    private Minecraft minecraft;
 
-    @Inject(method = "onKey", at = @At(value = "FIELD",
-            target = "Lnet/minecraft/client/option/GameOptions;hudHidden:Z"), cancellable = true)
+    @Inject(method = "keyPress", at = @At(value = "FIELD",
+            target = "Lnet/minecraft/client/Options;hideGui:Z"), cancellable = true)
     public void onF1Key(CallbackInfo ci) {
         BetterF1.state = BetterF1.state.next();
 
         // Seems most safe
-        client.options.hudHidden = !BetterF1.state.equals(HUDState.ALL_VISIBLE);
+        minecraft.options.hideGui = !BetterF1.state.equals(HUDState.ALL_VISIBLE);
         ci.cancel();
     }
 }
